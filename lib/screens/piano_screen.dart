8be3d73ab0626c766e57d32faa9bf18212000a27
tasneemwael <tasneem.wael119@gmail.com>
2024-10-29 22:25:13 +0300

    code edits

--- lib/screens/piano_screen.dart
+++ lib/screens/piano_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 class PianoScreen extends StatelessWidget {
-  const PianoScreen({super.key});
+  PianoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -10,134 +10,67 @@ class PianoScreen extends StatelessWidget {
       backgroundColor: Colors.grey,
       body: Stack(
         children: [
-          // White Keys Row
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
+            children: List.generate(
+              8,
+              (index) => GestureDetector(
                 onTap: () {
                   final player = AudioPlayer();
-                  player.play(AssetSource('audios/a6-102820.mp3'));
+                  player.play(AssetSource('audios/${notes[index]}.mp3'));
                 },
                 child: buildWhiteKey(),
               ),
-              GestureDetector(
-                onTap: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('audios/b6-82017.mp3'));
-                },
-                child: buildWhiteKey(),
-              ),
-              GestureDetector(
-                onTap: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('audios/c6-102822.mp3'));
-                },
-                child: buildWhiteKey(),
-              ),
-              GestureDetector(
-                onTap: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('audios/d6-82020.mp3'));
-                },
-                child: buildWhiteKey(),
-              ),
-              GestureDetector(
-                onTap: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('audios/do-80236.mp3'));
-                },
-                child: buildWhiteKey(),
-              ),
-              GestureDetector(
-                onTap: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('audios/f6-102819.mp3'));
-                },
-                child: buildWhiteKey(),
-              ),
-              GestureDetector(
-                onTap: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('audios/fa-78409.mp3'));
-                },
-                child: buildWhiteKey(),
-              ),
-              GestureDetector(
-                onTap: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('audios/g6-82013.mp3'));
-                },
-                child: buildWhiteKey(),
-              ),
-              GestureDetector(
-                onTap: () {
-                  final player = AudioPlayer();
-                  player.play(AssetSource('audios/g6-82014.mp3'));
-                },
-                child: buildWhiteKey(),
-              ),
-            ],
+            ),
           ),
           Positioned(
             top: 0,
             child: Row(
-              children: [
-                SizedBox(width: 67,),
-                GestureDetector(
+              children: List.generate(
+                5,
+                (index) => GestureDetector(
                   onTap: () {
                     final player = AudioPlayer();
-                    player.play(AssetSource('audios/music-box-note-14514.mp3'));
+                    player.play(AssetSource('audios/${blackNotes[index]}.mp3'));
                   },
-                  child: buildBlackKey(),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: index == 0 ? 67 : 118,
+                      ),
+                      buildBlackKey(),
+                      SizedBox(
+                        width: index == 0 ? 20 : 0,
+                      ),
+                    ],
+                  ),
                 ),
-                SizedBox(width: 118,),
-                GestureDetector(
-                  onTap: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('audios/piano-g-6200.mp3'));
-                  },
-                  child: buildBlackKey(),
-                ),
-                SizedBox(width: 20,),
-                GestureDetector(
-                  onTap: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('audios/re-78500.mp3'));
-                  },
-                  child: buildBlackKey(),
-                ),
-                SizedBox(width: 20,),
-                GestureDetector(
-                  onTap: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('audios/si-80238.mp3'));
-                  },
-                  child: buildBlackKey(),
-                ),
-                SizedBox(width: 118),
-                GestureDetector(
-                  onTap: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('audios/sol-101774.mp3'));
-                  },
-                  child: buildBlackKey(),
-                ),
-                SizedBox(width: 20,),
-                GestureDetector(
-                  onTap: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource('audios/tired-ghosts-piano-65013.mp3'));
-                  },
-                  child: buildBlackKey(),
-                ),
-              ],
+              ),
             ),
           ),
         ],
       ),
     );
   }
+
+  final List<String> notes = [
+    'a6-102820',
+    'b6-82017',
+    'c6-102822',
+    'd6-82020',
+    'do-80236',
+    'f6-102819',
+    'fa-78409',
+    'g6-82013',
+  ];
+
+  final List<String> blackNotes = [
+    'music-box-note-14514',
+    'piano-g-6200',
+    're-78500',
+    'si-80238',
+    'tired-ghosts-piano-65013',
+  ];
   Widget buildWhiteKey() {
     return Container(
       height: 400,
@@ -145,19 +78,20 @@ class PianoScreen extends StatelessWidget {
       decoration: BoxDecoration(
         color: Colors.white,
         border: Border.all(color: Colors.black, width: 5),
-        borderRadius: BorderRadius.only(
+        borderRadius: const BorderRadius.only(
           bottomRight: Radius.circular(8),
           bottomLeft: Radius.circular(8),
         ),
       ),
     );
   }
+
   Widget buildBlackKey() {
     return Container(
       height: 250,
       width: 60,
-      margin: EdgeInsets.only(left: 10, right: 10),
-      decoration: BoxDecoration(
+      margin: const EdgeInsets.only(left: 10, right: 10),
+      decoration: const BoxDecoration(
         color: Colors.black,
         borderRadius: BorderRadius.only(
           bottomRight: Radius.circular(5),
